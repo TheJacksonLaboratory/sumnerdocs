0cccad6d154d3ab9da6d3d8bf7266bcec6d9319b
Samir B Amin <sbamin@alumni.bcm.edu>
2020-02-26 16:48:32 -0500

    mkdocs config and build command

---
+++ build_methods.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+## strict check
+set -euo pipefail
+
+DOCDIR="$HOME"/Dropbox/acad/scripts/github/sumner
+
+if [[ ! -d "$DOCDIR" || ! -x "$DOCDIR" ]]; then
+	echo -e "\nERROR: DOCDIR does not exists or not accesible at $DOCDIR\n" >&2
+	exit 1
+fi
+
+## build docs
+cd "$DOCDIR" && \
+mkdocs build --clean && echo -e "\nINFO: Built updated docs\n"
+## mkdocs gh-deploy --clean -m "published using commit: {sha} and mkdocs {version}"
+
+## END ##
